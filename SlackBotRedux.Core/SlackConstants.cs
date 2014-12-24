c73f2b7de24307ae35ffd1552052ecaae44dd8b3
mohammedh123 <mohammedh123@gmail.com>
2014-12-23 22:35:52 -0500

    Adds SlackConstants to the Core project.

---
+++ SlackBotRedux.Core/SlackConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackBotRedux.Core
+{
+    public static class SlackConstants
+    {
+        public const string SlackOAuth2Url = "https://slack.com/oauth/authorize";
+    }
+}
