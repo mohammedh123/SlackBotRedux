4d867ac05174d10a319b308685f10029347ce3c1
mohammedh123 <mohammedh123@gmail.com>
2014-12-25 13:03:52 -0500

    Adds a JsonConverter for converting event types to their enum equivalents.

---
+++ SlackBotRedux.Core/EventTypeEnumConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SlackBotRedux.Core.Models;
+
+namespace SlackBotRedux.Core
+{
+    public class EventTypeEnumConverter : StringEnumConverter
+    { 
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var enumString = (string) reader.Value;
+
+            var enumVals = Enum.GetNames(typeof(EventType));
+            var match =
+                enumVals.FirstOrDefault(
+                    str => str.Equals(enumString.Replace("_", String.Empty), StringComparison.InvariantCultureIgnoreCase));
+            if (match == null) {
+                return null;
+            }
+
+            return Enum.Parse(typeof(EventType), match);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(EventType);
+        }
+    }
+}
