d17d3d588f123d96079758f412885acaa193a497
mohammedh123 <mohammedh123@gmail.com>
2014-12-25 13:49:20 -0500

    Adds a SubMessages file to hold submessages; adds the UserChangeMessage to SubMessages.

---
+++ SlackBotRedux.Core.Models/SubMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackBotRedux.Core.Models
+{
+    public class UserChangeMessage
+    {
+        public EventType Type { get; set; }
+
+        public User User { get; set; }
+    }
+}
