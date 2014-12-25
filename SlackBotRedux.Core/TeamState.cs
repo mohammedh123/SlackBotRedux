251b3254e45c3616a422b548cdf93dfb5b840ca2
mohammedh123 <mohammedh123@gmail.com>
2014-12-25 13:49:39 -0500

    Adds a new class for tracking relevant team state to the Core project.

---
+++ SlackBotRedux.Core/TeamState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlackBotRedux.Core.Models;
+
+namespace SlackBotRedux.Core
+{
+    /// <summary>
+    /// Contains information about the state of a Slack team.
+    /// Currently only contains useful information for a basic bot, such as a list of users and their ids/names.
+    /// </summary>
+    public class TeamState
+    {
+        public IDictionary<string, User> UsersById { get; set; } 
+    }
+}
