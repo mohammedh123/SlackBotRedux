840fc585b7b8fc25da0f8c2adbcb6b255cf567d3
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-03 23:37:02 -0500

    Adds the bare shell of a QuotesModule.

---
+++ SlackBotRedux.Core.Modules/QuotesModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackBotRedux.Core.Modules
+{
+    public class QuotesModule
+    {
+        public QuotesModule(IBotRegistrar botRegistrar)
+        {
+        }
+    }
+}
