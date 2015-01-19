57c4643de3dae94b5f86fc60b9c7eabdc21e8487
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-19 14:21:30 -0500

    Adds an ErrorMessages class to act as an error message resource. Not sure if this is a good idea or not.

---
+++ SlackBotRedux.Core/ErrorMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlackBotRedux.Core
+{
+    public static class ErrorMessages
+    {
+        public static string NoUserExistsForUsername(string username)
+        {
+            return String.Format("Sorry {0}, but no user exists with that username.", username);
+        }
+    }
+}
