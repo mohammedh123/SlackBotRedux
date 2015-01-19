fad9550bb16a8365a7127e246debff3e517ab869
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-19 14:14:40 -0500

    Adds a Response class for responding to messages from listener callbacks.

---
+++ SlackBotRedux.Core/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SlackBotRedux.Core
+{
+    public class Response
+    {
+        public BotMessage Message { get; set; }
+        public Match Match { get; set; }
+
+        private readonly IBot _bot;
+        
+        public Response(IBot bot, BotMessage message, Match match)
+        {
+            _bot = bot;
+            Message = message;
+            Match = match;
+        }
+
+        public void Send(string msg, params object[] paramz)
+        {
+            var textMsg = Message as TextInputBotMessage;
+            if (textMsg == null) return;
+
+            _bot.Send(textMsg.Message.Channel, String.Format(msg, paramz));
+        }
+    }
+}
