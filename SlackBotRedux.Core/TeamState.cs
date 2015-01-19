60043d0b39eedba8bb3a3e4255c4f967d24d72d7
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-19 14:14:11 -0500

    Updates TeamState and extracts an interface out (so users dont have to trawl through the dictionary themselves).

--- SlackBotRedux.Core/TeamState.cs
+++ SlackBotRedux.Core/TeamState.cs
@@ -7,12 +7,31 @@ using SlackBotRedux.Core.Models;
 
 namespace SlackBotRedux.Core
 {
+    public interface ITeamState
+    {
+        User GetUserByUsername(string username);
+        User GetUserByUserId(string userId);
+    }
+
     /// <summary>
     /// Contains information about the state of a Slack team.
     /// Currently only contains useful information for a basic bot, such as a list of users and their ids/names.
     /// </summary>
-    public class TeamState
+    public class TeamState : ITeamState
     {
-        public IDictionary<string, User> UsersById { get; set; } 
+        internal IDictionary<string, User> UsersById { get; set; }
+
+        public User GetUserByUsername(string username)
+        {
+            return UsersById.FirstOrDefault(kvp => String.Equals(kvp.Value.Name, username)).Value;
+        }
+
+        public User GetUserByUserId(string userId)
+        {
+            User user;
+            UsersById.TryGetValue(userId, out user);
+
+            return user;
+        }
     }
 }
