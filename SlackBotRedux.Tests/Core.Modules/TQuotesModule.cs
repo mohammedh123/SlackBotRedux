02e4ccba3ea89f98175df04cd540386e8870fcf4
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-22 23:57:43 -0500

    Adds a test file and a test for QuotesModule.

---
+++ SlackBotRedux.Tests/Core.Modules/TQuotesModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SlackBotRedux.Core;
+using SlackBotRedux.Core.Data;
+using SlackBotRedux.Core.Models;
+using SlackBotRedux.Core.Modules;
+
+namespace SlackBotRedux.Tests.Core.Modules
+{
+    [TestClass]
+    public class TQuotesModule
+    {
+        private const string BotName = "milkbot";
+        protected QuotesModule Subject;
+
+        protected Mock<IRecentMessageRepository> RecentMessageRepository;
+        protected Mock<IBot> Bot;
+        protected Mock<IMessageSender> MessageSender;
+
+        [TestInitialize]
+        public virtual void InitializeMocks()
+        {
+            RecentMessageRepository = new Mock<IRecentMessageRepository>();
+        }
+
+        public void InitializeSubject()
+        {
+            Subject = new QuotesModule(RecentMessageRepository.Object);
+        }
+
+        [TestClass]
+        public class RegisterToBot : TQuotesModule
+        {
+            [TestMethod]
+            public void ShouldRespondWithErrorBecauseOfBadUsername()
+            {
+                TestRespondToWithMessage(BotName + ", remember Kaladin storming",
+                    mock => mock
+                        .Setup(ts => ts.GetUserByUsername(It.IsAny<string>()))
+                        .Returns((User) null));
+
+                MessageSender.Verify(ims => ims.EnqueueOutputMessage(It.IsAny<string>(), ErrorMessages.NoUserExistsForUsername("Kaladin")));
+            }
+
+            private void TestRespondToWithMessage(string msgText, Action<Mock<ITeamState>> teamStateSetupFunc )
+            {
+                // Setup
+                MessageSender = new Mock<IMessageSender>();
+                InitializeSubject();
+                var msg = new TextInputBotMessage(new InputMessage
+                {
+                    Channel = "somewhere",
+                    Text = msgText
+                });
+
+                // Act
+                var bot = new Bot(BotName, MessageSender.Object);
+                var mockTeamState = new Mock<ITeamState>();
+                teamStateSetupFunc(mockTeamState);
+                bot.TeamState = mockTeamState.Object;
+
+                Subject.RegisterToBot(bot);
+                bot.ReceiveMessage(msg);
+            }
+        }
+    }
+}
