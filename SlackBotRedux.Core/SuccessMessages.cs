79d268df8d6a04864cc5e0a650d807daa1cf24d8
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-31 19:05:13 -0500

    Adds a new static class for successful messages.

---
+++ SlackBotRedux.Core/SuccessMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlackBotRedux.Core
+{
+    public static class SuccessMessages
+    {
+        public static string SuccessfulRemember(string username, string textToBeRemembered)
+        {
+            return String.Format("Okay, {0}, remembering \"{1}.\"", username, textToBeRemembered);
+        }
+    }
+}
