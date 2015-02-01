8a1df89bc5b63d7f986e86de867c729583b73275
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-31 19:06:52 -0500

    Adds a SQL implementation of IQuoteRepository.

---
+++ SlackBotRedux.Core.Data.Sql/QuoteRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using Dapper.Contrib.Extensions;
+using SlackBotRedux.Core.Data.Interfaces;
+using SlackBotRedux.Core.Models;
+
+namespace SlackBotRedux.Core.Data.Sql
+{
+    public class QuoteRepository : IQuoteRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public QuoteRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public Quote InsertNewQuote(string userId, string text)
+        {
+            var quote = new Quote {Text = text, Timestamp = DateTimeOffset.UtcNow, UserId = userId};
+            quote.Id = (int)_conn.Insert(quote);
+
+            return quote;
+        }
+    }
+}
