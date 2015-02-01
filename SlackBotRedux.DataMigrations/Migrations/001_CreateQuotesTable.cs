b77421f76dbf35c70b5ca5c5e3c17c906af79136
Mohammed Hossain <mohammedh123@gmail.com>
2015-02-01 01:40:32 -0500

    Adds a new project for database migrations.

---
+++ SlackBotRedux.DataMigrations/Migrations/001_CreateQuotesTable.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace SlackBotRedux.DataMigrations.Migrations
+{
+    [Migration(001)]
+    public class CreateQuotesTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Quotes")
+                  .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                  .WithColumn("UserId").AsString()
+                  .WithColumn("Text").AsString()
+                  .WithColumn("Timestamp").AsCustom("datetimeoffset");
+        }
+    }
+}
