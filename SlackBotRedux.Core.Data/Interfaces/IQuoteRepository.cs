2c3dd2670c3c23ca398292cf468a48af16ac7f58
Mohammed Hossain <mohammedh123@gmail.com>
2015-01-31 19:04:58 -0500

    Adds a new repository interface for quotes.

---
+++ SlackBotRedux.Core.Data/Interfaces/IQuoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using SlackBotRedux.Core.Models;
+
+namespace SlackBotRedux.Core.Data.Interfaces
+{
+    public interface IQuoteRepository
+    {
+        Quote InsertNewQuote(string userId, string text);
+    }
+}
