66c19da53832aa27a5d54a8e4366dadb85d06b14
Mohammed Hossain <mohammedh123@gmail.com>
2015-02-01 19:19:11 -0500

    Adds a DataMigrator class to the DataMigrations project.

---
+++ SlackBotRedux.DataMigrations/DataMigrator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors.SqlServer;
+
+namespace SlackBotRedux.DataMigrations
+{
+    public class MigrationOptions : IMigrationProcessorOptions
+    {
+        public bool PreviewOnly { get; set; }
+        public string ProviderSwitches { get; set; }
+        public int Timeout { get; set; }
+    }
+
+    public class DataMigrator
+    {
+        private readonly string _connectionString;
+
+        public DataMigrator(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void MigrateDatabase(Action<IMigrationRunner> runnerFunc)
+        {
+            var announcer = new NullAnnouncer();
+            var migrationsAssembly = Assembly.GetExecutingAssembly();
+
+            var migrationContext = new RunnerContext(announcer);
+            var options = new MigrationOptions() { PreviewOnly = false, Timeout = 0 };
+            var factory = new SqlServerProcessorFactory();
+            using (var processor = factory.Create(_connectionString, announcer, options))
+            {
+                var runner = new MigrationRunner(migrationsAssembly, migrationContext, processor);
+                runnerFunc(runner);
+                processor.CommitTransaction();
+            }
+        }
+    }
+}
