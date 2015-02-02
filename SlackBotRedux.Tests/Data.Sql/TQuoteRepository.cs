42e8da2184f80ef50c35e6b53a878d1b1c9de685
Mohammed Hossain <mohammedh123@gmail.com>
2015-02-01 19:20:14 -0500

    Adds tests for QuoteRepository and adds an App.config with a test db connection string.

---
+++ SlackBotRedux.Tests/Data.Sql/TQuoteRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Dapper;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlackBotRedux.Core.Models;
+using SlackBotRedux.Data.Sql;
+
+namespace SlackBotRedux.Tests.Data.Sql
+{
+    [TestClass]
+    internal class TQuoteRepository : DatabaseTest
+    {
+        protected QuoteRepository Subject;
+
+        [TestInitialize]
+        public void InitializeSubject()
+        {
+            Subject = new QuoteRepository(Connection);
+        }
+
+        [TestClass]
+        public class InsertNewQuote : TQuoteRepository
+        {
+            [TestMethod]
+            public void ShouldSucceedInsertingQuote()
+            {
+                // Act
+                const string userId = "userId";
+                const string quoteText = "text goes here";
+                Subject.InsertNewQuote(userId, quoteText);
+
+                // Verify
+                var quote = Connection.Query<Quote>("SELECT * FROM dbo.Quotes WHERE UserId = @userId", new {userId}).SingleOrDefault();
+                quote.Should().NotBeNull();
+                quote.UserId.Should().Be(userId);
+                quote.Text.Should().Be(quoteText);
+            }
+        }
+    }
+}
