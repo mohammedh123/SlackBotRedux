645e1043196aa1fcbd53ee4468f89d2b88727d96
Mohammed Hossain <mohammedh123@gmail.com>
2015-02-07 13:59:22 -0500

    Adds a migration for many-to-many variable groupings.

---
+++ SlackBotRedux.DataMigrations/Migrations/004_CreateVariableGroupingsTable.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace SlackBotRedux.DataMigrations.Migrations
+{
+    [Migration(004)]
+    public class CreateVariableGroupingsTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("VariableGroupings")
+                  .WithColumn("VariableId").AsInt32().ForeignKey("Variables", "Id")
+                  .WithColumn("GroupId").AsInt32().ForeignKey("Groups", "Id");
+        }
+    }
+}
