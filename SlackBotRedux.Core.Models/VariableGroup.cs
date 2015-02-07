5fc0b36ed623dba095595f9e5d83f1e28cce4da7
Mohammed Hossain <mohammedh123@gmail.com>
2015-02-07 13:58:20 -0500

    Adds a model and a migration for variable groups.

---
+++ SlackBotRedux.Core.Models/VariableGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace SlackBotRedux.Core.Models
+{
+    public class VariableGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SlackBotRedux.DataMigrations/Migrations/002_CreateVariableGroupsTable.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace SlackBotRedux.DataMigrations.Migrations
+{
+    [Migration(002)]
+    public class CreateVariableGroupsTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("VariableGroups")
+                  .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                  .WithColumn("Name").AsString(50);
+        }
+    }
+}
