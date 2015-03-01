d195b5143a383dfc83c43d500ce52895a1339730
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 02:36:52 -0500

    Updates VariableRepository with some more not-implemented methods and updates it to use the IVariableConfiguration instead of the ConfigurationManager.

--- SlackBotRedux.Data.Sql/VariableRepository.cs
+++ SlackBotRedux.Data.Sql/VariableRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using SlackBotRedux.Configuration;
 using SlackBotRedux.Core.Variables;
 using SlackBotRedux.Core.Variables.Interfaces;
 
@@ -11,9 +12,9 @@ namespace SlackBotRedux.Data.Sql
         private readonly VariableDictionary _variableCache;
         private readonly IDbConnection _conn;
 
-        public VariableRepository(IDbConnection conn)
+        public VariableRepository(IDbConnection conn, IVariableConfiguration config)
         {
-            _variableCache = new VariableDictionary("$", "[a-zA-Z-_]");
+            _variableCache = new VariableDictionary(config.PrefixString, config.AllowedNameCharactersRegex);
             _conn = conn;
 
             LoadVariableData();
@@ -21,14 +22,13 @@ namespace SlackBotRedux.Data.Sql
 
         private void LoadVariableData()
         {
-            throw new NotImplementedException();
         }
 
-        public bool AddVariable(string variableName)
+        public bool AddVariable(string variableName, bool isProtected)
         {
             throw new NotImplementedException();
         }
-
+        
         public VariableDefinition GetVariable(string variableName)
         {
             throw new NotImplementedException();
@@ -50,6 +50,11 @@ namespace SlackBotRedux.Data.Sql
         }
 
         public TryRemoveValueResult TryRemoveValue(string variableName, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetVariableProtection(string variableName, bool isProtected)
         {
             throw new NotImplementedException();
         }
