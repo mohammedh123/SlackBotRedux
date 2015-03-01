21ee8d9c1c7ea36a8ffc966f0730047812488397
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 01:59:08 -0500

    Adds a VariableConfigurationModule to the AutofacModules project.

---
+++ SlackBotRedux.AutofacModules/VariableConfigurationModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Autofac;
+using SlackBotRedux.Configuration;
+
+namespace SlackBotRedux.AutofacModules
+{
+    public class VariableConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(context =>
+            {
+                var settings = new VariableConfiguration()
+                {
+                    AllowedNameCharactersRegex = ConfigurationManager.AppSettings["Variable.AllowedNameCharactersRegex"],
+                    PrefixString = ConfigurationManager.AppSettings["Variable.PrefixString"]
+                };
+
+                return settings;
+            }).SingleInstance();
+        }
+    }
+}
