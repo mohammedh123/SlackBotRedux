17ec2c8e0b90a4dfdf1b6fac61f3cea34f1c417c
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 01:58:54 -0500

    Adds a VariableConfiguration class/interface to the Configuration project.

---
+++ SlackBotRedux.Configuration/VariableConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace SlackBotRedux.Configuration
+{
+    public interface IVariableConfiguration
+    {
+        string PrefixString { get; set; }
+        string AllowedNameCharactersRegex { get; set; }
+    }
+
+    public class VariableConfiguration : IVariableConfiguration
+    {
+        public string PrefixString { get; set; }
+        public string AllowedNameCharactersRegex { get; set; }
+    }
+}
