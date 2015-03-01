cd22394f90a6305fe8e41439ac584f0fa54ee49d
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 01:26:07 -0500

    Adds a BotConfiguration class/interface to the Configuration project.

---
+++ SlackBotRedux.Configuration/BotConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace SlackBotRedux.Configuration
+{
+    public interface IBotConfiguration
+    {
+        string ApiToken { get; set; }
+        string Name { get; set; }
+    }
+
+    public class BotConfiguration : IBotConfiguration
+    {
+        public string ApiToken { get; set; }
+        public string Name { get; set; }
+    }
+}
