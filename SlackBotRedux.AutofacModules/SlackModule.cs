5ccf9337e37187abf9cbab48f8b4d6a1697af518
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 02:09:56 -0500

    Adds a SlackModule to the AutofacModules configuration.

---
+++ SlackBotRedux.AutofacModules/SlackModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using SlackBotRedux.Core;
+
+namespace SlackBotRedux.AutofacModules
+{
+    public class SlackModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SlackClient>().SingleInstance().AsImplementedInterfaces();
+        }
+    }
+}
