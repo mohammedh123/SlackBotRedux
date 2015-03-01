7034c2fee56d7bfee57064444c1a105a111e903d
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 13:42:13 -0500

    Adds the missing VariableValue model.

---
+++ SlackBotRedux.Data.Models/VariableValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SlackBotRedux.Data.Models
+{
+    public class VariableValue
+    {
+        public int VariableId { get; set; }
+        public string Value { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
