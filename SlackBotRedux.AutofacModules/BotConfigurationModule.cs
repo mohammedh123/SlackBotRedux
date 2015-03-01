3c69ed6397c8654ed49e0b7dcdb4f9a692269019
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 01:27:07 -0500

    Adds a BotConfigurationModule to the AutofacModules project.

---
+++ SlackBotRedux.AutofacModules/BotConfigurationModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Autofac;
+using SlackBotRedux.Configuration;
+
+namespace SlackBotRedux.AutofacModules
+{
+    public class BotConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(context =>
+            {
+                var settings = new BotConfiguration
+                {
+                    ApiToken = ConfigurationManager.AppSettings["BotApiToken"],
+                    Name = ConfigurationManager.AppSettings["BotName"]
+                };
+
+                return settings;
+            }).SingleInstance();
+        }
+    }
+}
