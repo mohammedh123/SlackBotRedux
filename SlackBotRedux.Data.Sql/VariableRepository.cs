cdc0caa76a47b922e9192558fe7d463060f42f12
Mohammed Hossain <mohammedh123@gmail.com>
2015-03-01 13:40:14 -0500

    Implements LoadVariableData in VariableRepository.

--- SlackBotRedux.Data.Sql/VariableRepository.cs
+++ SlackBotRedux.Data.Sql/VariableRepository.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using NLog;
 using SlackBotRedux.Configuration;
 using SlackBotRedux.Core.Variables;
 using SlackBotRedux.Core.Variables.Interfaces;
+using SlackBotRedux.Data.Models;
 
 namespace SlackBotRedux.Data.Sql
 {
     public class VariableRepository : IVariableDictionary
     {
+        private readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private readonly VariableDictionary _variableCache;
         private readonly IDbConnection _conn;
-
+        
         public VariableRepository(IDbConnection conn, IVariableConfiguration config)
         {
             _variableCache = new VariableDictionary(config.PrefixString, config.AllowedNameCharactersRegex);
@@ -22,6 +28,33 @@ namespace SlackBotRedux.Data.Sql
 
         private void LoadVariableData()
         {
+            Logger.Info("Loading variable data from database.");
+
+            const string getAllVariablesSql = @"
+                SELECT Id, Name, IsProtected, CreatedDate, LastModifiedDate FROM dbo.Variables;
+                SELECT VariableId, Value, CreatedDate FROM dbo.VariableValues;
+            ";
+
+            List<Variable> variables;
+            List<VariableValue> variableValues;
+            using (var gridReader = _conn.QueryMultiple(getAllVariablesSql)) {
+                variables = gridReader.Read<Variable>().ToList();
+                variableValues = gridReader.Read<VariableValue>().ToList();
+            }
+
+            variables.ForEach(v => _variableCache.AddVariable(v.Name, v.IsProtected));
+            variableValues.ForEach(vv =>
+            {
+                var matchingVar = variables.FirstOrDefault(v => v.Id == vv.VariableId);
+                if (matchingVar == null) {
+                    Logger.Warn("Found variable value in database without matching variable, with variable id {0} and value = \"{1}\".", vv.VariableId, vv.Value);
+                    return;
+                }
+
+                _variableCache.TryAddValue(matchingVar.Name, vv.Value);
+            });
+
+            Logger.Info("Finished loading variable data from database.");
         }
 
         public bool AddVariable(string variableName, bool isProtected)
